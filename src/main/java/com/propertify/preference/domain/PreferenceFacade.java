4eb51f13827a8ad942f1b76bf6e5fb90ecc4bc1d
Maksymilian Ulanecki <maks.u@vp.pl>
2024-06-07 17:33:00 +0200

    implemented facade methods

--- src/main/java/com/propertify/preference/domain/PreferenceFacade.java
+++ src/main/java/com/propertify/preference/domain/PreferenceFacade.java
@@ -1,4 +1,47 @@
 package com.propertify.preference.domain;
 
+import com.propertify.preference.dto.PreferenceDto;
+import com.propertify.preference.dto.PreferenceRequestDto;
+import com.propertify.preference.exception.PreferencesNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class PreferenceFacade {
+
+    private final PreferenceRepository preferenceRepository;
+    private final RangeMapper rangeMapper;
+    private final PreferenceUpdater preferenceUpdater;
+
+    PreferenceDto getPreferencesByUserId(Long userId) {
+        return preferenceRepository
+                .findByUserId(userId)
+                .orElseThrow(() -> new PreferencesNotFoundException(userId))
+                .toDto();
+    }
+
+    PreferenceDto addNewPreferences(PreferenceRequestDto preferenceRequestDto) {
+
+        final Preference newPreference = Preference.builder()
+                .priceRange(rangeMapper.priceRangeFromDto(
+                        preferenceRequestDto.priceRange()))
+                .metricAreaRange(rangeMapper.metricAreaRangeFromDto(
+                        preferenceRequestDto.metricAreaRange()))
+                .cityName(PreferencesValuesValidator
+                        .validateCityName(preferenceRequestDto.cityName()))
+                .build();
+
+        return preferenceRepository.save(newPreference).toDto();
+    }
+
+    PreferenceDto updatePreferences(PreferenceRequestDto preferenceRequestDto) {
+
+        final Preference preferenceToUpdate = preferenceRepository
+                .findByUserId(preferenceRequestDto.userId())
+                .orElseThrow();
+
+        preferenceUpdater.updatePreferences(preferenceToUpdate, preferenceRequestDto);
+
+        return preferenceRepository.save(preferenceToUpdate).toDto();
+    }
+
 }
